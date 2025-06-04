e849dfcfc37e2eec4189d09a398a43a3c91be8b1
Nick Morin <morinnick31@gmail.com>
2025-06-04 15:21:51 +0300

    add footer desktop version

--- js/script.js
+++ js/script.js
@@ -11,3 +11,10 @@ const workBlockSwiper = new Swiper(".work-block__slider-swiper", {
     prevEl: ".work-block__swiper-button-prev",
   },
 });
+
+const modalMenuDesktop = document.querySelector("#modal-menu-desktop");
+const modalMenuBtn = document.querySelector("#modal-menu-btn");
+
+modalMenuBtn.addEventListener("click", () => {
+  modalMenuDesktop.classList.add("open-menu");
+});
